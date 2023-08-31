80c536b327de055f5828f6135affd01619347bd6
DigitalDeveloperAM <achrafmehenneche.am@gmail.com>
2023-08-31 22:05:06 +0200

    Completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,34 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  let spliting = word.split("");
+  let reversWord = spliting.reverse();
+  let joined = reversWord.join("");
+  if (word === joined) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
+
+// isPalindrome("mom");
 
 /* 
   Add your pseudocode here
+  if the word === to word
+  spliting letters
+  revers them
+  joined them
+  compere word with joined
+
 */
 
 /*
   Add written explanation of your solution here
+  assign the word letters in array using split method to splite the letters,
+  and using revers mothod to revers letters, and using join method to rejoin 
+  what has been reversed and compers word with the end joined array or word 
+  using if condition.
 */
 
 // You can run `node index.js` to view these console logs
